733c4d8fc84bd5399e4cd288ab743e5bae6ecf3c
Morshed-Ahmed <morshed50r@gmail.com>
2021-10-19 15:35:29 +0600

    code update lasts

--- src/components/Home/ServiceDtl/ServiceDtl.js
+++ src/components/Home/ServiceDtl/ServiceDtl.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './ServiceDtl.css'
 import { useParams } from 'react-router';
 
 const ServiceDtl = () => {
@@ -15,8 +16,8 @@ const ServiceDtl = () => {
     const newItem = data.filter(td => td._id == serviceDtls);
     return (
 
-        <div className=" mx-auto mt-5">
-            <img src={newItem[0]?.imageURL} alt="" />
+        <div className=" mx-auto mt-5 container w-50 detail-style">
+            <img className="mt-5" src={newItem[0]?.imageURL} alt="" />
             <h4>{newItem[0]?.name}</h4>
             <p>{newItem[0]?.description}</p>
 
